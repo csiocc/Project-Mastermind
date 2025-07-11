59af575f1a68e99b94129b4a4690d7c021eed3a0
Christian <christian.siegrist@open-circle.ch>
2025-07-11 11:24:31 +0200

    added KI guess Logic, extracted compare function into an own Module called HintCalc and improved it

--- main.rb
+++ main.rb
@@ -1,12 +1,37 @@
 require "tty-prompt"
-Debugging = true
+Debugging = false
 
 
-module MasterMind
+module MasterMind #Module für Ki Hinweisberechnung (Inspired by  https://rosettacode.org/ Python Mastermind & Gemini )
 
-  
-  
+  module HintCalc #Module for Hintcalculation
+    def self.calculate(s_code, guess_arr)
+      temp_secret = s_code.dup
+      temp_guess = guess_arr.dup
 
+      provisional_hints = []
+
+      s_code.length.times do |i|  #mark right pos/num
+        if temp_guess[i] == temp_secret[i]
+          provisional_hints << "X"
+          temp_secret[i] = nil # delete used nums
+          temp_guess[i] = nil  # delete used nums
+        end
+      end
+
+      temp_guess.each_with_index do |char, i|
+        next unless char # skip nil/X
+          secret_idx = temp_secret.index(char)
+        if secret_idx
+          provisional_hints << "O"
+          temp_secret[secret_idx] = nil # delete used nums
+        end
+      end
+      remaining_slots = s_code.length - provisional_hints.length
+      remaining_slots.times { provisional_hints << "-" }
+      provisional_hints.sort_by { |h| h == "X" ? 0 : (h == "O" ? 1 : 2) }.join("")
+    end
+  end
 
   class Setter
     attr_reader :code, :hint
@@ -18,7 +43,7 @@ module MasterMind
     end
 
     def get_code
-      puts "Insert your Code a Number between 1111 - 6666 or 'exit' to leave"
+      puts "Insert your Code a Number between 1111 - 6666"
       @code = gets.chomp.chars.to_a
     end
     
@@ -27,57 +52,85 @@ module MasterMind
     end
 
     def display_hint(guess)
-      self.compare(guess)
+      @hint = HintCalc.calculate(@code, guess)
       @hint
-    
-    end
-
-    def compare(guess)
-      temp_code = @code.dup
-      temp_guess = guess.dup
-      hint = Array.new(@code.length, "")
-
-      @code.length.times do |i| #check for num&pos match
-        if temp_guess [i] == temp_code[i]
-          hint[i] = "X"
-          temp_code[i] = nil #delete used num
-          temp_guess[i] = nil
-        end
-      end
-
-      temp_guess.each_with_index do |char, i| #check remaining chars for num match
-        next unless char #skip removed chars
-        code_i = temp_code.index(char)
-        if code_i
-          hint[i] = "O"
-          temp_code[code_i] = nil #delete used num
-        else
-          hint[i] = "-" #mark - if not O / X
-        end
-      end
-
-      hint.each_with_index do |value, i|
-        if value.empty? #nil's replaced by "-" 
-          hint[i]
-        end
-      end
-
-      @hint = hint.join("")
-    end
-    
+    end  
   end
   
 
   class Guesser 
-    attr_reader :guess
-    attr_accessor :player_role
+    attr_reader :guess, :player_role
+    attr_accessor :possible_codes
+
     def initialize(guess, player_role)
       @guess = guess
       @player_role = player_role
+      @possible_codes = generate_codes
+      @first_guess_made = false
     end
 
-    def ki_make_guess
-      @guess = "1111".chars.to_a
+    def generate_codes
+      codes = []
+      (1..6).each do |a|
+        (1..6).each do |b|
+          (1..6).each do |c|
+            (1..6).each do |d|
+              codes << [a.to_s, b.to_s, c.to_s, d.to_s]
+            end
+          end
+        end
+      end
+      codes
+    end
+
+    def calculate_hint(s_code, guess_arr)
+      HintCalc.calculate(s_code, guess_arr)
+    end
+
+    def ki_make_guess(last_guess = nil, last_hint = nil)
+      puts "DEBUG: aktuelle mögliche Codes: #{@possible_codes.length}" if Debugging
+
+      unless @first_guess_made
+        @guess = ["1", "1", "2", "2"] #first guess allways 1122
+        @first_guess_made = true
+        return @guess
+      end
+
+      if last_guess && last_hint
+        @possible_codes.select! do |code|
+          calculated_hint = calculate_hint(code, last_guess)
+          calculated_hint == last_hint
+        end
+        puts "DEBUG: mögliche codes nach filterung: #{@possible_codes.length}" if Debugging
+      end
+
+      if @possible_codes.length == 1
+        @guess = @possible_codes.first
+      else
+        best_guess = nil
+        max_eliminated_in_worst_case = -1
+
+        candidate_guesses = @possible_codes
+
+        candidate_guesses.each do |candidate_guess|
+          hint_counts = Hash.new(0)
+
+          @possible_codes.each do |possible_secret|
+            hint = calculate_hint(possible_secret, candidate_guess) 
+            hint_counts[hint] += 1
+          end
+
+          worst_case_size = hint_counts.values.max || 0 
+
+          if best_guess.nil? || worst_case_size < max_eliminated_in_worst_case
+            max_eliminated_in_worst_case = worst_case_size
+            best_guess = candidate_guess
+          end
+        end
+        @guess = best_guess
+      end
+      
+      @guess
     end
 
     def make_guess 
@@ -104,7 +157,7 @@ module MasterMind
 
     loop do  #Main Game Loop
       prompt = TTY::Prompt.new #chose role
-      player_choice_input = prompt.select("Choose your destiny?") do |menu|
+      player_choice_input = prompt.select("Choose your Role") do |menu|
         menu.choice "Set-Code"
         menu.choice "Guess-Code"
       end
@@ -124,7 +177,7 @@ module MasterMind
 
       if setter_one.player_role 
         setter_one.get_code
-      else
+      else #Ki is setter
         setter_one.ki_get_code
       end
      
@@ -133,9 +186,13 @@ module MasterMind
       current_round_tries = 0
 
       puts "Round: #{total_rounds_played} | AVG Tries: #{MasterMind.avg_tries(total_rounds_played - 1, total_tries_sum).round(1)}"
-      puts "Insert your Guess a Number between 1111 - 6666 or 'exit' to leave"
+      puts "Insert your Guess a Number between 1111 - 6666 or 'exit' to leave" if guesser_one.player_role
+      puts "Calculating......" if setter_one.player_role
 
-      max_tries = 5
+      max_tries = 12
+      last_ki_guess = nil #storage for ki guess
+      last_ki_hint = nil #storage for ki hint
+
       loop do # Round Game Loop
 
         if current_round_tries >= max_tries # game stops after 12 trys
@@ -147,10 +204,15 @@ module MasterMind
         if guesser_one.player_role
           guesser_one.make_guess
           guess = guesser_one.guess.to_a
-        else
-          guesser_one.ki_make_guess
-          guess = guesser_one.ki_make_guess.to_a
+        else #Ki is guesser
+          guesser_one.ki_make_guess(last_ki_guess, last_ki_hint)
+          guess = guesser_one.ki_make_guess
+          last_ki_guess = guess #stores ki guess
+          puts "KI-Guess: #{guess.join}"
         end
+
+        hint = setter_one.display_hint(guess)
+        last_ki_hint = hint
 
         if guess.join.downcase == "exit"
           puts "Exiting, cu!"
@@ -161,10 +223,19 @@ module MasterMind
         p setter_one.display_hint(guess)
         p "actual code: #{setter_one.code.join}" if Debugging
 
+        unless still_no_win?(setter_one, guess)
+          puts "Ki - got it in #{current_round_tries+1} Trys! This Ki is so bad, or your Code is insane." if setter_one.player_role && current_round_tries+1 > 4 && current_round_tries+1 > 8
+          puts "Ki - got it in #{current_round_tries+1} Trys! Still good." if setter_one.player_role && current_round_tries+1 > 4 && current_round_tries+1 < 8
+          puts "Ki - got it in #{current_round_tries+1} Trys Awesome not? Your Code is a bit too easy." if setter_one.player_role && current_round_tries+1 <= 4
+          puts "Ki - got it in #{current_round_tries+1} Trys! This was bad." if guesser_one.player_role && current_round_tries+1 > 4 && current_round_tries+1 > 8
+          puts "Ki - got it in #{current_round_tries+1} Trys! Still good." if guesser_one.player_role && current_round_tries+1 > 4 && current_round_tries+1 < 8
+          puts "GZ! u got it in #{current_round_tries+1} Trys Awesome! Your're a crack.'" if guesser_one.player_role && current_round_tries+1 <= 4
+        end
+
         if MasterMind.still_no_win?(setter_one, guess)
           current_round_tries += 1
         else
-          puts "GZ! U got it in #{current_round_tries} Trys"
+          puts "GZ! U got it in #{current_round_tries+1} Trys" if guesser_one.player_role
           total_tries_sum += (current_round_tries + 1)
           break #Stop gamecycle player win
         end
